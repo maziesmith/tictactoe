eb100a98e28ba318c5527fef1fba7588929c4d44
RAdministrator <admin@example.com>
2019-09-08 22:32:33 -0400

    2nd commit

--- app/src/main/java/com/example/tic_tac_toe/Score.java
+++ app/src/main/java/com/example/tic_tac_toe/Score.java
@@ -1,13 +1,18 @@
 package com.example.tic_tac_toe;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.app.AlertDialog;
+
 
 import com.example.rodger.tictactoe.R;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,9 +30,35 @@ public class Score extends StartNewGame {
         List<String> nameArr = new ArrayList<String>();
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nameArr);
 
+
         //Append match score and names to the score list
-        nameArr.add(getIntent().getExtras().getString("firstplayername") + " wins: " + getIntent().getExtras().getInt("player1score") + " vs " + getIntent().getExtras().getString("secondplayername") + " wins: " + getIntent().getExtras().getInt("player2score"));
-        score_lv.setAdapter(arrayAdapter);
+        try {
+            if(getIntent().getExtras().getString("firstplayername").length()>0 && getIntent().getExtras().getString("secondplayername").length()>0 && getIntent().getExtras().getInt("player1score") >= 0 && getIntent().getExtras().getInt("player2score")>=0) {
+
+                nameArr.add(getIntent().getExtras().getString("firstplayername") + " wins: " + getIntent().getExtras().getInt("player1score") + " vs " + getIntent().getExtras().getString("secondplayername") + " wins: " + getIntent().getExtras().getInt("player2score"));
+                score_lv.setAdapter(arrayAdapter);
+            }else{
+nameArr.add("No scores to display");
+                score_lv.setAdapter(arrayAdapter);
+            }
+
+        }catch (Exception e){
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setTitle("Something went wrong...");
+//            StringWriter sw = new StringWriter();
+//            e.printStackTrace(new PrintWriter(sw));
+//            String exceptionAsString = sw.toString();
+//            alertDialog.setMessage(exceptionAsString);
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+
+        }
+
 
         //Clear Scores button
         final Button clearButton = findViewById(R.id.ClearScorebutton);
